bb1add65bee49b0a21f742923e3ae23f160d2766
TalmazanLucia <lucydr470@gmail.com>
2024-12-09 14:24:00 +0200

    Initial commit

---
+++ app/config.py
@@ -0,0 +1,11 @@
+import uuid
+
+class Config:
+    CASSANDRA_HOSTS = ['cassandra1']
+    CASSANDRA_PORT = 9042
+    CASSANDRA_KEYSPACE = 'clothes_app'
+    NODE_ID = str(uuid.uuid4())
+    CACHE_TYPE = 'RedisCache'
+    CACHE_REDIS_HOST = 'redis'
+    CACHE_REDIS_PORT = 6379
+    CACHE_DEFAULT_TIMEOUT = 300
